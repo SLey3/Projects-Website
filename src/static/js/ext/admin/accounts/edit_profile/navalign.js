2bbf148d30a268201739670168a5cc9e04fa6844
Sergio Ley <ghub4127@gmail.com>
2021-01-16 11:02:54 -0800

    Edit Profile: 75% Completion | Accounts Management 39% Completion | added more css with :hover transistions

--- src/static/js/ext/admin/accounts/edit_profile/navalign.js
+++ src/static/js/ext/admin/accounts/edit_profile/navalign.js
@@ -9,19 +9,29 @@ document.addEventListener('mousemove', function checkNavHover() {
         if (hovered) {
             const container = document.getElementById('cntnr');
             const seperators = document.getElementsByName("sep");
+            const infTitle = document.getElementsByName("sub-title");
             container.style.marginLeft = "13.67rem";
             for (s = 0; s < seperators.length; s++) {
                 let seperator = seperators[s];
                 seperator.style.background = "rgb(44, 44, 44)";
             }
+            for (i = 0; i < infTitle.length; i++) {
+                let title = infTitle[i];
+                title.style.whiteSpace = "nowrap";
+            }
 
         } else {
             const container = document.getElementById('cntnr');
             const seperators = document.getElementsByName('sep');
+            const infTitle = document.getElementsByName("sub-title");
             container.style.marginLeft = '-10rem';
             for (s = 0; s < seperators.length; s++) {
                 let seperator = seperators[s];
                 seperator.style.background = "rgb(128, 128, 128)";
+            }
+            for (i = 0; i < infTitle.length; i++) {
+                let title = infTitle[i];
+                title.style.whiteSpace = "pre-line";
             }
         }
     }
